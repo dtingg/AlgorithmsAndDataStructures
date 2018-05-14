a24706332c32907bc96adbd74e343d5b16b805b1
Dianna Tingg <dtingg@uw.edu>
2018-05-14 16:59:57 -0700

    initial

---
+++ hw5_1.py
@@ -0,0 +1,33 @@
+"""
+Homework 5.1
+Recall the palindrome exercise from quiz 4:
+A palindrome is a word or a phrase that has the same characters
+forwards and backwards. For example, "never odd or even",
+"racecar" are palindromes.
+Given a string containing a word or sequence of words, write a
+function that determines if the string is a palindrome. You can
+assume that there is no punctuation and all characters are lowercase.
+Implement a function is_palindrome(chars) that takes as input a list
+of character strings. For example:
+racecar = ['r', 'a', 'c', 'e', 'c', 'a', 'r']
+should_be_true = is_palindrome(racecar)
+"""
+
+
+# Use recursion to see if a list of characters is a palindrone
+def is_palindrone(chars):
+    if len(chars) < 2:
+        return True
+    elif chars[0] != chars[-1]:
+        return False
+    else:
+        is_palindrone(chars[1:-1])
+    return True
+
+print('Is "dianna" a palindrone?')
+dianna = ["d", "i", "a", "n", "n", "a"]
+print(is_palindrone(dianna))
+
+print('\nIs "racecar" a palindrone?')
+racecar = ["r", "a", "c", "e", "c", "a", "r"]
+print(is_palindrone(racecar))

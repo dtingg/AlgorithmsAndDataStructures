beb407040958d4db0d580b00d40c0b4eee5e035f
Dianna Tingg <dtingg@uw.edu>
2018-05-15 19:00:59 -0700

    initial

---
+++ hw5_5.py
@@ -0,0 +1,19 @@
+"""
+Homework 5.5
+Convert this iterative function to be recursive. Same
+idea as the previous question — think it through, maybe
+use some paper and pencil, and convert it directly.
+
+def iterer(n):
+    x = 1
+    while n > 1:
+        x += 1
+        n = n / 2
+    return x
+"""
+
+
+def recurer(n):
+    if n < 2:
+        return 1
+    return 1 + recurer(n / 2)

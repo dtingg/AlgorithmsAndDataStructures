db75767fb9c2975ec30237ae492526996d1f0f5c
Dianna Tingg <dtingg@uw.edu>
2018-05-20 14:55:16 -0700

    updated formatting

--- q6_1.py
+++ q6_1.py
@@ -1,12 +1,15 @@
 """
 Quiz 6
 Question 1
-Write a function, fizzbuzz that prints the numbers from 1 to 100. For each multiple of 3, print “Fizz” instead of the number. For each multiple of 5, print “Buzz” instead of the number. For numbers which are multiples of both 3 and 5, print “FizzBuzz” instead of the number.
+Write a function, fizzbuzz that prints the numbers from 1 to 100.
+For each multiple of 3, print “Fizz” instead of the number.
+For each multiple of 5, print “Buzz” instead of the number.
+For numbers which are multiples of both 3 and 5, print “FizzBuzz” instead of the number.
 """
 
 
 def fizzbuzz():
-    for n in range (1, 101):
+    for n in range(1, 101):
         if n % 5 == 0 and n % 3 == 0:
             print(n, "FizzBuzz")
         elif n % 3 == 0:
